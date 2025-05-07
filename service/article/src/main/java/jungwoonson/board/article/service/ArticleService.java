b3ee9b11e901d62f60ec25ad9d43a1d6cb886725
jungwoonson <jungwoon727@gmail.com>
2025-05-07 19:27:49 +0900

    게시글 CRUD 추가

---
+++ service/article/src/main/java/jungwoonson/board/article/service/ArticleService.java
@@ -0,0 +1,43 @@
+package jungwoonson.board.article.service;
+
+import jakarta.transaction.Transactional;
+import jungwoonson.board.article.entity.Article;
+import jungwoonson.board.article.repository.ArticleRepository;
+import jungwoonson.board.article.service.request.ArticleCreateRequest;
+import jungwoonson.board.article.service.request.ArticleUpdateRequest;
+import jungwoonson.board.article.service.response.ArticleResponse;
+import jungwoonson.board.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final Snowflake snowflake = new Snowflake();
+    private final ArticleRepository articleRepository;
+
+    @Transactional
+    public ArticleResponse create(ArticleCreateRequest request) {
+        Article article = articleRepository.save(
+                Article.create(snowflake.nextId(), request.getTitle(), request.getContent(), request.getBoardId(), request.getWriterId())
+        );
+        return ArticleResponse.from(article);
+    }
+
+    @Transactional
+    public ArticleResponse update(Long articleId, ArticleUpdateRequest request) {
+        Article article = articleRepository.findById(articleId).orElseThrow();
+        article.update(request.getTitle(), request.getContent());
+        return ArticleResponse.from(article);
+    }
+
+    @Transactional
+    public ArticleResponse read(Long articleId) {
+        return ArticleResponse.from(articleRepository.findById(articleId).orElseThrow());
+    }
+
+    @Transactional
+    public void delete(Long articleId) {
+        articleRepository.deleteById(articleId);
+    }
+}
