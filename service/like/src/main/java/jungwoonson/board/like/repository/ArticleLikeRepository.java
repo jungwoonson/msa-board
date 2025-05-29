717e0ebc6d31be223a308b68836560aba70c45c2
jungwoonson <jungwoon727@gmail.com>
2025-05-29 15:54:48 +0900

    좋아요 CRD 구현

---
+++ service/like/src/main/java/jungwoonson/board/like/repository/ArticleLikeRepository.java
@@ -0,0 +1,11 @@
+package jungwoonson.board.like.repository;
+
+import java.util.Optional;
+import jungwoonson.board.like.entity.ArticleLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
+    Optional<ArticleLike> findByArticleIdAndUserId(Long articleId, Long userId);
+}
---
+++ service/like/src/main/java/jungwoonson/board/like/service/response/ArticleLikeResponse.java
@@ -0,0 +1,24 @@
+package jungwoonson.board.like.service.response;
+
+import java.time.LocalDateTime;
+import jungwoonson.board.like.entity.ArticleLike;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class ArticleLikeResponse {
+    private Long articleLikeId;
+    private Long articleId;
+    private Long userId;
+    private LocalDateTime createdAt;
+
+    public static ArticleLikeResponse from(ArticleLike articleLike) {
+        ArticleLikeResponse response = new ArticleLikeResponse();
+        response.articleLikeId = articleLike.getArticleLikeId();
+        response.articleId = articleLike.getArticleId();
+        response.userId = articleLike.getUserId();
+        response.createdAt = articleLike.getCreatedAt();
+        return response;
+    }
+}
