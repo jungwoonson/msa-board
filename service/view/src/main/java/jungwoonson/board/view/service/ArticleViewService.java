be07d449bdf2cd5b815f0257df4a08fc1065d215
jungwoonson <jungwoon727@gmail.com>
2025-06-11 17:55:37 +0900

    조회수 구현 & 레디스 추가

---
+++ service/view/src/main/java/jungwoonson/board/view/service/ArticleViewService.java
@@ -0,0 +1,26 @@
+package jungwoonson.board.view.service;
+
+import jungwoonson.board.view.repository.ArticleViewCountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleViewService {
+
+    private final ArticleViewCountRepository articleViewCountRepository;
+    private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+    private static final int BACK_UP_BATCH_SIZE = 100;
+
+    public Long increase(Long articleId, Long userId) {
+        Long count = articleViewCountRepository.increase(articleId);
+        if (count % BACK_UP_BATCH_SIZE == 0) {
+            articleViewCountBackUpProcessor.backUp(articleId, count);
+        }
+        return count;
+    }
+
+    public Long count(Long articleId) {
+        return articleViewCountRepository.read(articleId);
+    }
+}
